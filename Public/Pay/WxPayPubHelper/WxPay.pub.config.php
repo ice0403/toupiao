a6efa40c4093a8862df8065d80d7ccdd3f2c5bea
crazydouble <crazydouble@sina.com>
2016-09-18 16:16:36 +0800

    1.修改报名支付信息 2.修改证书

--- Public/Pay/WxPayPubHelper/WxPay.pub.config.php
+++ Public/Pay/WxPayPubHelper/WxPay.pub.config.php
@@ -7,27 +7,27 @@ class WxPayConf_pub
 {
 	//=======【基本信息设置】=====================================
 	//微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
-	const APPID = 'wxf022fc156026a511';
+	const APPID = 'wx620c9b3a1d326631';
 	//受理商ID，身份标识
-	const MCHID = '1226922402';
+	const MCHID = '1266184501';
 	//商户支付密钥Key。审核通过后，在微信发送的邮件中查看
-	const KEY = 'd32831100d1bf387d8faec783fde3111';
+	const KEY = '9tW2p7Y3Ysj5F097j1bTQyFzvOzC6UKW';
 	//JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
-	const APPSECRET = 'd32831100d1bf387d8faec783fde3102';
+	const APPSECRET = '23c3daa8c3d4d494d314b587c08bac91';
 
 	//=======【JSAPI路径设置】===================================
 	//获取access_token过程中的跳转uri，通过跳转将code传入jsapi支付页面
 	//const JS_API_CALL_URL = 'http://20160627.zz.lanrenmb.com/Public/Pay/demo/js_api_call.php';
-	const JS_API_CALL_URL = 'http://20160713b.zz.lanrenmb.com/Home/index.php';
+	const JS_API_CALL_URL = 'http://toupiao.meishuquan.net/Home/index.php';
 
 	//=======【证书路径设置】=====================================
 	//证书路径,注意应该填写绝对路径
-	const SSLCERT_PATH = '/home/www/Guest/20160713b.zz.lanrenmb.com/Public/Pay/WxPayPubHelper/cacert/apiclient_cert.pem';
-	const SSLKEY_PATH = '/home/www/Guest/20160713b.zz.lanrenmb.com/Public/Pay/WxPayPubHelper/cacert/apiclient_key.pem';
+	const SSLCERT_PATH = '/data/wwwroot/toupiao.meishuquan.net/Public/Pay/WxPayPubHelper/cacert/apiclient_cert.pem';
+	const SSLKEY_PATH = '/data/wwwroot/toupiao.meishuquan.net/Public/Pay/WxPayPubHelper/cacert/apiclient_key.pem';
 
 	//=======【异步通知url设置】===================================
 	//异步通知url，商户根据实际开发过程设定
-	const NOTIFY_URL = 'http://20160713b.zz.lanrenmb.com/Public/Pay/demo/notify_url.php';
+	const NOTIFY_URL = 'http://toupiao.meishuquan.net/Public/Pay/demo/notify_url.php';
 
 	//=======【curl超时设置】===================================
 	//本例程通过curl使用HTTP POST方法，此处可修改其超时时间，默认为30秒
