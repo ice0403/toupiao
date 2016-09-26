f812813a2075835e0d059c2b1f3c5d3f723a4b98
crazydouble <crazydouble@sina.com>
2016-09-26 17:52:34 +0800

    手机验证码

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -1616,8 +1616,12 @@ public function payVote(){
 
 			$content = '恭喜您已获得金笔奖入场票【1张】，购票码为：【'.$send['ticket_code'].'】。请您保存好此短信，当日持短信进入会场。';
 			$this->sms_sending_copy($send['tel'],$content);
-		}	
-        $this->success('提交成功！',U("Index/index",array("id"=>$this->_param('id'))));
+			$this->success('支付成功！购票码为：【'.$send['ticket_code'].'】，稍后会收到短信凭证，请耐心等待！',U("Index/index",array("id"=>$this->_param('id'))));
+		}else{
+			$this->success('提交成功,！',U("Index/index",array("id"=>$this->_param('id'))));
+		}
+
+        
         exit;
 
 }
