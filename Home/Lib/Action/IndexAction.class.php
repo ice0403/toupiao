eb5a9c1f6709bf3f185a4edb1687de606ce7487a
crazydouble <crazydouble@sina.com>
2016-09-26 15:07:23 +0800

    手机验证码

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -1608,8 +1608,13 @@ public function payVote(){
         $Model = new Model();
         $result1 = $Model->execute($sql);
 		$send = M("Form")->where('id='.$_GET['vid'])->field('tel,ticket_code')->find();
-
-		$this->sms_sending_copy($send->tel,'恭喜您参与金笔奖购票活动，您的购票码为：'.$send->ticket_code.'。请您保存好该短信，持短信入场。');
+		
+		$content = '恭喜您参与金笔奖购票活动，您的购票码为：'.$send->ticket_code.'。请您保存好该短信，持短信入场。';
+		echo $send->tel;
+		echo "<br>";
+		echo $content;
+		exit;
+		$this->sms_sending_copy($send->tel,$content);
 					
         $this->success('提交成功！',U("Index/index",array("id"=>$this->_param('id'))));
         exit;
